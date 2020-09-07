b8577d4f1085114d2e189fe9a35b0926e19415cd
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-09-06 21:43:55 -0500

    added comparison check for guess and random gen number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("WELCOME TO THE GUESSING GAME");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please enter a number.");
 
@@ -10,5 +15,13 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Unable to read your guess...");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a guess number");
     println!("You guessed the number: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Equal => println!("Your guess of {} was correct!", guess),
+        Ordering::Less => println!("Your guess of {} was less than the random number", guess),
+        Ordering::Greater => println!("Your guess of {} was greater than the random number", guess),
+    }
 }
