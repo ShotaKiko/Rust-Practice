101018f83103795a3e7460344434c1ebd5aaced2
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-09-06 22:05:00 -0500

    added error handling for invalid guess user input

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,26 @@ fn main() {
     println!("WELCOME TO THE GUESSING GAME");
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
+    //uncomment to test with secret value known
+
+    //Game Loop
     loop {
-        println!("Please enter a your guess.");
+        println!("Please enter your guess.");
+        //Creating new string to store user guess
+        //
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Unable to read your guess...");
-        let guess: u32 = guess.trim().parse().expect("Please type a guess number");
+        //Potential error handling
+        //
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, //invalid user input not needed for game purposes
+        };
+        //Logging user Input
+        //
         println!("You guessed the number: {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Your guess of {} was less than the random number", guess),
