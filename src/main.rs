302af5d46fcd5b139157b5d2936732650007c142
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-09-06 21:50:28 -0500

    added game loop with exit condition on win match

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,24 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
-
-    println!("Please enter a number.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Unable to read your guess...");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a guess number");
-    println!("You guessed the number: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Equal => println!("Your guess of {} was correct!", guess),
-        Ordering::Less => println!("Your guess of {} was less than the random number", guess),
-        Ordering::Greater => println!("Your guess of {} was greater than the random number", guess),
+    loop {
+        println!("Please enter a your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Unable to read your guess...");
+        let guess: u32 = guess.trim().parse().expect("Please type a guess number");
+        println!("You guessed the number: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess of {} was less than the random number", guess),
+            Ordering::Greater => {
+                println!("Your guess of {} was greater than the random number", guess)
+            }
+            Ordering::Equal => {
+                println!("Your guess of {} was correct! YOU WIN", guess);
+                println!("Now exiting the game...");
+                break;
+            }
+        }
     }
 }
